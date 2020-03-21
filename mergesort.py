6e7ff8db220c7583b435160164a29d02ad2ecd13
Paras Jain <57864667+paras-jain@users.noreply.github.com>
2020-03-21 22:44:38 +0530

    Merge Sort implementation

---
+++ mergesort.py
@@ -0,0 +1,43 @@
+def mergesort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        l = arr[:mid]
+        r = arr[mid:]
+        
+        mergesort(l)
+        mergesort(r)
+        
+        i = j = k = 0
+        
+        while i < len(l) and j < len(r):
+            if l[i] < r[j]: 
+                    arr[k] = l[i] 
+                    i+=1
+            else: 
+                arr[k] = r[j] 
+                j+=1
+            k+=1
+        while i < len(l): 
+            arr[k] = l[i] 
+            i+=1
+            k+=1
+          
+        while j < len(r): 
+            arr[k] = r[j] 
+            j+=1
+            k+=1
+            
+
+def printList(arr): 
+    for i in range(len(arr)):         
+        print(arr[i],end=" ") 
+    print() 
+    
+list = []
+n = input("enter length: ")
+for i in range(0, int(n)):
+    k=input("enter value: ")
+    list.append(k)
+    
+mergesort(list)
+printList(list)
